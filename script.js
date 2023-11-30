b18a1ccd69ca1138361449da8a384b34130abcf9
Dima-Kutaini <deemaKutainy@gmail.com>
2023-11-30 22:14:12 +0100

    create services section

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-const menuIcon=document.querySelector(".menu-icon");
+/** @format */
+
+const menuIcon = document.querySelector('.menu-icon');
 const xBtn = document.querySelector('.x-btn');
 const navigation = document.querySelector('.navigation');
 
-
-menuIcon.addEventListener("click", ()=>{
-    navigation.classList.add("navigate"); 
-}); 
-
+menuIcon.addEventListener('click', () => {
+  navigation.classList.add('navigate');
+});
 
 xBtn.addEventListener('click', () => {
-    navigation.classList.remove('navigate');
-  });
+  navigation.classList.remove('navigate');
+});
 
-window.addEventListener("scroll",()=>{
-    const about= document.querySelector(".about")
-    if(window.pageYOffset>= 200){
-        about.classList.add("change")
+window.addEventListener('scroll', () => {
+  const about = document.querySelector('.about');
+  const services = document.querySelector('.services');
+  if (window.pageYOffset >= 200) {
+    about.classList.add('change');
+  } else {
+    about.classList.remove('change');
+  }
+    if (window.pageYOffset >= about.offsetTop +200) {
+      services.classList.add('change');
+    } else {
+      services.classList.remove('change');
     }
-    else{
-        about.classList.remove("change")
-    }
-    console.log(window.pageYOffset)
-})
+console.log('about.offsetTop:', about.offsetTop);
+console.log('window.pageYOffset:', window.pageYOffset);
+
+});
