c5789b2463f45d43fe8044f278d0dcee81a8265c
Dima-Kutaini <deemaKutainy@gmail.com>
2023-11-30 13:26:51 +0100

    create about section

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ xBtn.addEventListener('click', () => {
     navigation.classList.remove('navigate');
   });
 
+window.addEventListener("scroll",()=>{
+    const about= document.querySelector(".about")
+    if(window.pageYOffset>= 200){
+        about.classList.add("change")
+    }
+    else{
+        about.classList.remove("change")
+    }
+    console.log(window.pageYOffset)
+})
