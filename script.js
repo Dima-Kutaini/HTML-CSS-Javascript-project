d8e4e4edbf8de4d1bdd7baff942018f7b47a7101
Dima-Kutaini <deemaKutainy@gmail.com>
2023-12-01 23:16:40 +0100

    create data section with counter

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const menuIcon = document.querySelector('.menu-icon');
 const xBtn = document.querySelector('.x-btn');
 const navigation = document.querySelector('.navigation');
+let start = false;
 
 menuIcon.addEventListener('click', () => {
   navigation.classList.add('navigate');
@@ -15,27 +16,47 @@ xBtn.addEventListener('click', () => {
 window.addEventListener('scroll', () => {
   const about = document.querySelector('.about');
   const services = document.querySelector('.services');
-const portfolio = document.querySelector('.portfolio');
+  const portfolio = document.querySelector('.portfolio');
+  const data = document.querySelector('.data');
+  const myData = document.querySelectorAll('.num');
 
-  if (window.pageYOffset >= 200) {
+  if (window.scrollY >= 200) {
     about.classList.add('change');
   } else {
     about.classList.remove('change');
   }
 
-    if (window.pageYOffset >= about.offsetTop + 200) {
-      services.classList.add('change');
+  if (window.scrollY >= about.offsetTop + 200) {
+    services.classList.add('change');
+  } else {
+    services.classList.remove('change');
+
+    if (window.scrollY >= services.offsetTop ) {
+      portfolio.classList.add('change');
     } else {
-      services.classList.remove('change');
-
-      if (window.pageYOffset >= services.offsetTop) {
-        portfolio.classList.add('change');
-      } else {
-        portfolio.classList.remove('change');
-      }
+      portfolio.classList.remove('change');
     }
+  }
 
+  if (window.scrollY >= data.offsetTop - 300) {
+    if (start) {
+      myData.forEach((num) => {
+        startCount(num);
+      });
+    }
+    start = true;
+  }
 });
+
+const startCount = (el) => {
+  let max = el.dataset.val;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent === max) {
+      clearInterval(count);
+    }
+  }, 1000);
+};
 
 
 
